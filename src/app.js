57b2da09169802c50c28bab87259ae3118e9bf87
Camille <cam.duplessis@gmail.com>
2022-06-26 17:21:05 +1000

    Adding search city

--- src/app.js
+++ src/app.js
@@ -42,12 +42,21 @@ function displayTemperature(response) {
     "src",
     `https//openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "cafbd7974d35bf96f37717d1116fdc4b";
-let city = "Brisbane";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+function search(city) {
+  let apiKey = "cafbd7974d35bf96f37717d1116fdc4b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
